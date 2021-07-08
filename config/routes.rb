7acf851c8b52c830dc5bbb828a66454ea76f02a4
EC2 Default User <ec2-user@ip-172-31-73-157.ec2.internal>
2021-07-08 18:51:32 +0000

    [Add]adminのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/admin" => "admin/homes#top"
+
+
+  namespace :admin do
+    get "/" => "homes#top", as: :root
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
+  end
 end
