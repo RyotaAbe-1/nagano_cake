161d8b7a11849d29f556c2166056c4d253c2d8d3
EC2 Default User <ec2-user@ip-172-31-73-157.ec2.internal>
2021-07-09 21:28:42 +0000

    [Add]商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,31 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+    @genre = Genre.all
+  end
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
   def index 
     @items = Item.all
   end
+  
+  def show
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+  
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path      
+    else
+      
+    end
+  end
 end
