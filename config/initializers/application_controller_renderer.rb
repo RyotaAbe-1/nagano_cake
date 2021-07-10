d4f7f4e6e48fc4bd81360901f5c38c889bae9d8a
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-07-10 01:39:26 +0000

    [Add]商品詳細画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '164aa731c45e83cff577623ab5d1d0fc5280c21aa63f497bd1eb27d1dcf3d12fcbbb3f60b654f43575bffb814f7d94aeac700b89d548bf23b5c4dc3dabe34c18'
