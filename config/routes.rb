b43a1b0d38b1a3806a1a17510ee965e0a2937584
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-07-10 14:29:24 +0000

    [Add]会員一覧、詳細、編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-    get "/" => "homes#top", as: :root
+    root "homes#top" 
     resources :items, except: [:destroy]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
