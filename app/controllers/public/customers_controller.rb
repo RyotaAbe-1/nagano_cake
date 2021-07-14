0b6c6d4756bb49cfbd029b9804e0b7e780c83a7a
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-07-14 21:05:24 +0000

    [Add]publicマイページ、退会画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,3 +1,26 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customer = Customer.find(current_customer.id)
+  end
   
+  def edit
+    
+  end
+  
+  def update
+  end
+
+  def confirm
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def withdraw
+    customer = Customer.find(current_customer.id)
+    customer.update(customer_params)
+    redirect_to root_path
+  end
+  
+  def customer_params
+    params.require(:customer).permit(:is_active)
+  end
 end
