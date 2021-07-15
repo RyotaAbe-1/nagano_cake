e6491549ce6b663d184fa16fec39e0c190ca78f3
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-07-15 13:09:28 +0000

    [Add]publicの登録情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,12 +2,15 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def edit
-    
+    @customer = Customer.find(current_customer.id)
   end
-  
+
   def update
+    customer = Customer.find(current_customer.id)
+    customer.update(customer_params)
+    redirect_to customers_path
   end
 
   def confirm
@@ -19,8 +22,8 @@ class Public::CustomersController < ApplicationController
     customer.update(customer_params)
     redirect_to root_path
   end
-  
+
   def customer_params
-    params.require(:customer).permit(:is_active)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
 end
