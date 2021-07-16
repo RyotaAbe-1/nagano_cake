774761c3874fe10f012a67b25e963cbbbb920e8b
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-07-16 19:53:57 +0000

    [Update]ログイン後、ログアウト後の遷移

--- app/controllers/publics/sessions_controller.rb
+++ app/controllers/publics/sessions_controller.rb
@@ -5,14 +5,15 @@ class Publics::SessionsController < Devise::SessionsController
   before_action :reject_inactive_customer, only: [:create]
 
   def reject_inactive_customer
-    customer =  Customer.find_by(email: params[:customer][:email])
-    if customer
-      if (customer.valid_password?(params[:customer][:password])) && (customer.is_active == false)
+    @customer =  Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if (@customer.valid_password?(params[:customer][:password])) && (@customer.is_active == false)
         flash[:error] = "退会済みです"
         redirect_to new_customer_registration_path
       end
     end
   end
+  
   # GET /resource/sign_in
   # def new
   #   super
